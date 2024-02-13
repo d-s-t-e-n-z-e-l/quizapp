6327905c73e581dbb04ea1478003a9bf03449a63
d-s-t-e-n-z-e-l <desiree.stenzel@gmx.de>
2024-02-13 13:56:31 +0100

    html & style playbuttons finished

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ let questions = [
         "answer_3": "Horatio",
         "answer_4": "Jaden",
         "lright_answer": "2",
-        "number_of_question": "1",
-       
+        "number_of_question": 1,
+
     },
     {
         "question": "Welcher Dinosaurier war der schnellste?",
@@ -18,7 +18,7 @@ let questions = [
         "answer_3": "Gallimimus",
         "answer_4": "Struthiomimus",
         "lright_answer": "4",
-        "number_of_question": "2",
+        "number_of_question": 2,
     },
     {
         "question": "Was bedeuted Ankylosaurus auf Deutsch?",
@@ -27,7 +27,7 @@ let questions = [
         "answer_3": "Echse ohne Flügel",
         "answer_4": "Breiter Riese",
         "lright_answer": "1",
-        "number_of_question": "3",
+        "number_of_question": 3,
     },
     {
         "question": "Wie heißt der Hybrid aus Jurassic Worl 1?",
@@ -36,7 +36,7 @@ let questions = [
         "answer_3": "Velociraptor",
         "answer_4": "Indominus Rex",
         "lright_answer": "4",
-        "number_of_question": "4",
+        "number_of_question": 4,
     },
     {
         "question": "Wie lang kein aus ausgewachsender Baryonyx sein?",
@@ -45,7 +45,7 @@ let questions = [
         "answer_3": "11m",
         "answer_4": "8m",
         "lright_answer": "3",
-        "number_of_question": "5",
+        "number_of_question": 5,
     },
     {
         "question": "Wo wurde Tristan aus Frage 1 ausgegraben?",
@@ -54,7 +54,7 @@ let questions = [
         "answer_3": "Halbinsel Koma",
         "answer_4": "Griechenland",
         "lright_answer": "1",
-        "number_of_question": "6",
+        "number_of_question": 6,
     },
     {
         "question": "In welcher erdgeschichtlichen Periode haben die meisten Sauropoden gelebt?",
@@ -63,7 +63,7 @@ let questions = [
         "answer_3": "Jura",
         "answer_4": "Kreide",
         "lright_answer": "3",
-        "number_of_question": "7",
+        "number_of_question": 7,
     },
     {
         "question": "Welche Art Dinosaurier ist Ducky aus Disneys In einem Lanf vor unserer Zeit?",
@@ -72,16 +72,16 @@ let questions = [
         "answer_3": "Stegosaurus",
         "answer_4": "Parasaurolophus",
         "lright_answer": "4",
-        "number_of_question": "8",
+        "number_of_question": 8,
     },
     {
-        "question": "Wie viele Jahre beherrschten die Dinisoaurier alle Kontinente?",
+        "question": "Wie viele Jahre beherrschten die Dinosoaurier alle Kontinente?",
         "answer_1": "140 Millionen Jahre",
         "answer_2": "300 Millionen Jahre",
         "answer_3": "90 Millionen Jahre",
         "answer_4": "10 Millionen Jahre",
         "lright_answer": "1",
-        "number_of_question": "9",
+        "number_of_question": 9,
     },
     {
         "question": "Welches Paar lebte erdzeitgeschichtlich am nächsten beieinandner?",
@@ -90,7 +90,31 @@ let questions = [
         "answer_3": "Mensch und Stegosaurus",
         "answer_4": "Stegosaurus und Endoceratus",
         "lright_answer": "1",
-        "number_of_question": "10",
+        "number_of_question": 10,
     },
 ]
+
+function startQuiz() {
+    runQuiz(1);
+}
+
+function runQuiz(questionNumber) {
+    renderQuestion(questionNumber);
+}
+
+function renderQuestion(questionNumber) {
+    let innerCard = document.getElementById("innerCard")
+
+    for (let i = 0; i < questions.length; i++) {
+        if (questionNumber === questions[i]["number_of_question"]) {
+            innerCard.innerHTML = questionTemplate(i);
+        }
+        
+    }
+
+    
+}
+
+
+
 
---
+++ templates.js
@@ -0,0 +1,28 @@
+function questionTemplate(i) {
+    return /*html*/`
+       <div> 
+            <section class="question-card">
+                    <h5 class="question fw-bold text-center w-75">${questions[i]['question']}</h5>
+                    <figure class="answerblock hoverAnswer">
+                        <div class="numberblock">1</div>
+                        <p class="answeroption">${questions[i]['answer_1']}</p>
+                    </figure>
+                    <figure class="answerblock hoverAnswer">
+                        <div class="numberblock">2</div>
+                        <p class="answeroption">${questions[i]['answer_2']}</p>
+                    </figure>
+                    <figure class="answerblock hoverAnswer">
+                        <div class="numberblock">3</div>
+                        <p class="answeroption">${questions[i]['answer_3']}</p>
+                    </figure>
+                    <figure class="answerblock hoverAnswer">
+                        <div class="numberblock">4</div>
+                        <p class="answeroption">${questions[i]['answer_4']}</p>
+                    </figure>
+            </section>
+            <figure class="buttonFigure"><button class="playButtons btn">Exit Game</button>
+                    <button class="playButtons btn">Next</button>
+            </figure>
+         </div>
+    `
+}
